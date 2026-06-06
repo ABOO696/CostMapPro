99320d143c877f5ef99314f41c5aeae4cbaf6405
ABOO696 <qoo00.tw@gmail.com>
2026-06-07 02:49:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import streamlit as st
 from app.sidebar import (
     render_sidebar
 )
-render_sidebar()
+
 st.set_page_config(
     page_title="CostMap Pro",
     layout="wide"
 )
 
-
+render_sidebar()
