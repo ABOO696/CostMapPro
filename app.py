d5e54b195e69edb439920fd8f0b3aabef7d9f93d
ABOO696 <qoo00.tw@gmail.com>
2026-06-07 02:20:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
+from app.sidebar import (
+    render_sidebar
+)
+
 st.set_page_config(
     page_title="CostMap Pro",
     layout="wide"
 )
 
+render_sidebar()
+
 st.title("📈 CostMap Pro")
 
-st.write("歡迎使用 CostMap Pro")
+st.write(
+    "歡迎使用 CostMap Pro"
+)
