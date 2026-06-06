036c49bbfe812e48e3a686ea2790588efd482a20
ABOO696 <qoo00.tw@gmail.com>
2026-06-07 02:44:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ st.set_page_config(
     page_title="CostMap Pro",
     layout="wide"
 )
-
-render_sidebar()
-
 st.title("📈 CostMap Pro")
 
 st.write(
     "歡迎使用 CostMap Pro"
+    
 )
+render_sidebar()
+
+
